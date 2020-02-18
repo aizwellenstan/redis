b038b58a2bba207656981280a8f1e1919985d162
aizwellenstan <aizwellenstan@gmail.com>
2020-02-18 14:41:27 +0900

    change mongo port

--- packages/server/src/hisSave.ts
+++ packages/server/src/hisSave.ts
@@ -49,9 +49,11 @@ var initialData = function () {
                 // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
                 body = JSON.parse(body)
-                for (var i = 0; i < body.length; i++) {
-                    delete body[i].Attributes
-                    body[i].Attributes = []
+                if(body.length > 0){
+                    for (var i = 0; i < body.length; i++) {
+                        delete body[i].Attributes
+                        body[i].Attributes = []
+                    }
                 }
 
                 var emptyAttr = JSON.stringify(body)
