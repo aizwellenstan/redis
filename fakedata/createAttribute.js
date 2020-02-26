802e341ce8b0ff740853c73b29a44d0ab6797779
aizwellenstan <aizwellenstan@gmail.com>
2020-02-26 14:51:30 +0900

    update

--- fakedata/createAttribute.js
+++ fakedata/createAttribute.js
@@ -14,7 +14,10 @@ function randomIntFromInterval(min, max, dec) {
   // min and max included
   console.log(dec)
   if(dec){
-    return Math.floor((Math.random() * (max - min + 1) + min)*10)/10;
+    if ((max-min)==0){
+      return min
+    }
+    return Math.round((Math.random() * (max - min + 1) + min)*10)/10;
   }
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
