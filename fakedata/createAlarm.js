f0fbcacda51189161a0cdf24436ea018dd92840f
aizwellenstan <aizwellenstan@gmail.com>
2020-03-17 19:54:12 +0900

    fix

--- fakedata/createAlarm.js
+++ fakedata/createAlarm.js
@@ -13,7 +13,7 @@ var fakeAlarm = {
   json: [
     {
       objectId: "f9ad22d7-4657-408b-95db-08147c619ae7",
-      DisplayObjectId: "environment_co100101", //CO感測器01
+      DisplayObjectId: "f9ad22d7-4657-408b-95db-08147c619ae7", //CO感測器01
       AlarmCategory: "Medium",
       AlarmFunction: "AlarmNeedAckNoReset",
       TriggerLogic: ">",
@@ -26,7 +26,7 @@ var fakeAlarm = {
     },
     {
       objectId: "b48ddf73-c31d-4731-8047-83ad40e73464",
-      DisplayObjectId: "environment_chillermotor000201", //冷卻水泵01 
+      DisplayObjectId: "1caa6a0e-fd7e-46d0-9f89-082bb96c36a6", //冷卻水泵01 
       AlarmCategory: "Low",
       AlarmFunction: "AlarmNoAckNoReset",
       TriggerLogic: "=",
@@ -50,22 +50,22 @@ var fakeAlarm = {
     //   AlarmMessageJapenese: "ACBB1-故障",
     //   IsOpenTrigger: "1"
     // },
-    {
-      objectId: "4bdc2e8a-ce5c-4a84-b0a8-8efe4d74e0ef",
-      DisplayObjectId: "environment_chillerwatertank000301", //冷水箱01 
-      AlarmCategory: "Medium",
-      AlarmFunction: "AlarmNeedAckNoReset",
-      TriggerLogic: "=",
-      trigger_value: "0",
-      AlarmMessage: "冷卻水塔-1停止中",
-      AlarmMessageEnglish: "冷卻水塔-1停止中",
-      AlarmMessageSpanish: "冷卻水塔-1停止中",
-      AlarmMessageJapenese: "冷卻水塔-1停止中",
-      IsOpenTrigger: "1"
-    },
+    // {
+    //   objectId: "4bdc2e8a-ce5c-4a84-b0a8-8efe4d74e0ef",
+    //   DisplayObjectId: "415cb5b8-3160-4956-b255-cc263b483df8", //冷水箱01 
+    //   AlarmCategory: "Medium",
+    //   AlarmFunction: "AlarmNeedAckNoReset",
+    //   TriggerLogic: "=",
+    //   trigger_value: "0",
+    //   AlarmMessage: "冷卻水塔-1停止中",
+    //   AlarmMessageEnglish: "冷卻水塔-1停止中",
+    //   AlarmMessageSpanish: "冷卻水塔-1停止中",
+    //   AlarmMessageJapenese: "冷卻水塔-1停止中",
+    //   IsOpenTrigger: "1"
+    // },
     {
       objectId: "8b13dfea-a7fb-4404-88a5-b58f5a3e5822",
-      DisplayObjectId: "environment_chillermotor000201", //冷卻水泵01 
+      DisplayObjectId: "1caa6a0e-fd7e-46d0-9f89-082bb96c36a6", //冷卻水泵01 
       AlarmCategory: "High",
       AlarmFunction: "AlarmNeedAckNeedReset",
       TriggerLogic: "=",
@@ -316,7 +316,7 @@ createAlarm = () => {
   });
   setInterval(function () {
     ddosAlarm_His("f9ad22d7-4657-408b-95db-08147c619ae7", 51, 60); // CO 感測器1
-    ddosAlarm_His("4bdc2e8a-ce5c-4a84-b0a8-8efe4d74e0ef", 0, 0); // 冷卻水塔-1運轉狀態
+    ddosAlarm_His("4bdc2e8a-ce5c-4a84-b0a8-8efe4d74e0ef-2", 0, 0); // 冷卻水塔-1運轉狀態
     // ddosAlarm_His("b48ddf73-c31d-4731-8047-83ad40e73464", 0, 0); // 冷卻馬達-1運轉狀態
     ddosAlarm_His("8b13dfea-a7fb-4404-88a5-b58f5a3e5822", 1, 1); // 冷卻馬達-1故障跳脫
     // ddosAlarm_His("e10f9d11-13b5-4694-ba12-cec88222cf20", 1, 1); // ACBB1-故障狀態01
